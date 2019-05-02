65ebc5398671db4c353e9cb6ed52db5828d25209
grey <fairingrey@gmail.com>
2019-05-01 17:38:10 -0700

    add decompressed stream support for deflate/zlib

--- src/stream/flate.rs
+++ src/stream/flate.rs
@@ -5,8 +5,8 @@ use core::{
 use std::io::Result;
 
 use bytes::{Bytes, BytesMut};
-use flate2::FlushCompress;
-pub use flate2::{Compress, Compression};
+use flate2::{FlushCompress, FlushDecompress};
+pub use flate2::{Compress, Compression, Decompress};
 use futures::{ready, stream::Stream};
 use pin_project::unsafe_project;
 
@@ -68,6 +68,68 @@ impl<S: Stream<Item = Result<Bytes>>> CompressedStream<S> {
             input_buffer: Bytes::new(),
             output_buffer: BytesMut::new(),
             compress,
+        }
+    }
+}
+
+#[unsafe_project(Unpin)]
+pub struct DecompressedStream<S: Stream<Item = Result<Bytes>>> {
+    #[pin]
+    inner: S,
+    flushing: bool,
+    input_buffer: Bytes,
+    output_buffer: BytesMut,
+    decompress: Decompress,
+}
+
+impl<S: Stream<Item = Result<Bytes>>> Stream for DecompressedStream<S> {
+    type Item = Result<Bytes>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<Bytes>>> {
+        const OUTPUT_BUFFER_SIZE: usize = 8_000;
+
+        let this = self.project();
+
+        if this.input_buffer.is_empty() {
+            if *this.flushing {
+                return Poll::Ready(None);
+            } else if let Some(bytes) = ready!(this.inner.poll_next(cx)) {
+                *this.input_buffer = bytes?;
+            } else {
+                *this.flushing = true;
+            }
+        }
+
+        this.output_buffer.resize(OUTPUT_BUFFER_SIZE, 0);
+
+        let flush = if *this.flushing {
+            FlushDecompress::Finish
+        } else {
+            FlushDecompress::None
+        };
+
+        let (prior_in, prior_out) = (this.decompress.total_in(), this.decompress.total_out());
+        this.decompress
+            .decompress(this.input_buffer, this.output_buffer, flush)?;
+        let input = this.decompress.total_in() - prior_in;
+        let output = this.decompress.total_out() - prior_out;
+
+        this.input_buffer.advance(input as usize);
+        Poll::Ready(Some(Ok(this
+            .output_buffer
+            .split_to(output as usize)
+            .freeze())))
+    }
+}
+
+impl<S: Stream<Item = Result<Bytes>>> DecompressedStream<S> {
+    pub fn new(stream: S, decompress: Decompress) -> DecompressedStream<S> {
+        DecompressedStream {
+            inner: stream,
+            flushing: false,
+            input_buffer: Bytes::new(),
+            output_buffer: BytesMut::new(),
+            decompress,
         }
     }
 }
--- src/stream/zlib.rs
+++ src/stream/zlib.rs
@@ -5,9 +5,9 @@ use core::{
 use std::io::Result;
 use std::marker::Unpin;
 
-use super::flate::CompressedStream;
+use super::flate::{CompressedStream, DecompressedStream};
 use bytes::Bytes;
-use flate2::Compress;
+use flate2::{Compress, Decompress};
 pub use flate2::Compression;
 use futures::{stream::Stream, stream::StreamExt};
 
@@ -27,6 +27,26 @@ impl<S: Stream<Item = Result<Bytes>> + Unpin> ZlibStream<S> {
     pub fn new(stream: S, level: Compression) -> ZlibStream<S> {
         ZlibStream {
             inner: CompressedStream::new(stream, Compress::new(level, true)),
+        }
+    }
+}
+
+pub struct DecompressedZlibStream<S: Stream<Item = Result<Bytes>> + Unpin> {
+    inner: DecompressedStream<S>,
+}
+
+impl<S: Stream<Item = Result<Bytes>> + Unpin> Stream for DecompressedZlibStream<S> {
+    type Item = Result<Bytes>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<Bytes>>> {
+        self.inner.poll_next_unpin(cx)
+    }
+}
+
+impl<S: Stream<Item = Result<Bytes>> + Unpin> DecompressedZlibStream<S> {
+    pub fn new(stream: S) -> DecompressedZlibStream<S> {
+        DecompressedZlibStream {
+            inner: DecompressedStream::new(stream, Decompress::new(true)),
         }
     }
 }

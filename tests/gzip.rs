78d09642517b725ba8a62d5e83026da84d8dbcb4
Wim Looman <wim@nemo157.com>
2019-05-03 13:26:23 +0200

    Add tests for writing chunks that should be >8k when compressed

--- tests/gzip.rs
+++ tests/gzip.rs
@@ -5,6 +5,7 @@ use futures::{
     stream::{self, StreamExt},
 };
 use std::io::{self, Read};
+use std::iter::FromIterator;
 
 #[test]
 fn gzip_stream() {
@@ -21,4 +22,29 @@ fn gzip_stream() {
     let mut output = vec![];
     GzDecoder::new(&data[..]).read_to_end(&mut output).unwrap();
     assert_eq!(output, vec![1, 2, 3, 4, 5, 6]);
+}
+
+#[test]
+fn gzip_stream_large() {
+    use async_compression::stream::gzip;
+
+    let bytes = [
+        Vec::from_iter((0..20_000).map(|_| rand::random())),
+        Vec::from_iter((0..20_000).map(|_| rand::random())),
+    ];
+
+    let stream = stream::iter(vec![
+        Bytes::from(bytes[0].clone()),
+        Bytes::from(bytes[1].clone()),
+    ]);
+    let compressed = gzip::GzipStream::new(stream.map(Ok), gzip::Compression::default());
+    let data: Vec<_> = block_on(compressed.collect());
+    let data: io::Result<Vec<_>> = data.into_iter().collect();
+    let data: Vec<u8> = data.unwrap().into_iter().flatten().collect();
+    let mut output = vec![];
+    GzDecoder::new(&data[..]).read_to_end(&mut output).unwrap();
+    assert_eq!(
+        output,
+        Vec::from_iter(bytes[0].iter().chain(bytes[1].iter()).cloned())
+    );
 }

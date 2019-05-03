f151917bc01bb8dc15693a7facf7267baf15b15c
grey <fairingrey@gmail.com>
2019-05-03 12:03:15 -0700

    fixup decompressed brotli/flate

--- src/stream/brotli.rs
+++ src/stream/brotli.rs
@@ -77,7 +77,7 @@ impl<S: Stream<Item = Result<Bytes>>> BrotliStream<S> {
 pub struct DecompressedBrotliStream<S: Stream<Item = Result<Bytes>>> {
     #[pin]
     inner: S,
-    flushing: bool,
+    flush: bool,
     decompress: Decompress,
 }
 
@@ -85,46 +85,45 @@ impl<S: Stream<Item = Result<Bytes>>> Stream for DecompressedBrotliStream<S> {
     type Item = Result<Bytes>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<Bytes>>> {
-//        const OUTPUT_BUFFER_SIZE: usize = 8_000;
-//
-//        let this = self.project();
-//
-//        if *this.flushing {
-//            return Poll::Ready(None);
-//        }
-//
-//        let input_buffer = if let Some(bytes) = ready!(this.inner.poll_next(cx)) {
-//            bytes?
-//        } else {
-//            *this.flushing = true;
-//            Bytes::new()
-//        };
-//
-//        let mut decompressed_output = BytesMut::with_capacity(OUTPUT_BUFFER_SIZE);
-//        let input_ref = &mut input_buffer.as_ref();
-//        let output_ref = &mut &mut [][..];
-//        loop {
-//            let status = this.decompress.decompress(input_ref, output_ref)?;
-//            while let Some(buf) = this.decompress.take_output(None) {
-//                decompressed_output.put(buf);
-//            }
-//            match status {
-//                DeStatus::Finished => break,
-//                DeStatus::NeedInput => {
-//                    if *this.flushing {
-//                        return Poll::Ready(Some(Err(Error::new(
-//                            ErrorKind::UnexpectedEof,
-//                            "reached unexpected EOF",
-//                        ))));
-//                    }
-//                    break;
-//                }
-//                DeStatus::NeedOutput => (),
-//            }
-//        }
-//
-//        Poll::Ready(Some(Ok(decompressed_output.freeze())))
-        unimplemented!()
+        const OUTPUT_BUFFER_SIZE: usize = 8_000;
+
+        let this = self.project();
+
+        if *this.flush {
+            return Poll::Ready(None);
+        }
+
+        let input_buffer = if let Some(bytes) = ready!(this.inner.poll_next(cx)) {
+            bytes?
+        } else {
+            *this.flush = true;
+            Bytes::new()
+        };
+
+        let mut decompressed_output = BytesMut::with_capacity(OUTPUT_BUFFER_SIZE);
+        let input_ref = &mut input_buffer.as_ref();
+        let output_ref = &mut &mut [][..];
+        loop {
+            let status = this.decompress.decompress(input_ref, output_ref)?;
+            while let Some(buf) = this.decompress.take_output(None) {
+                decompressed_output.extend_from_slice(buf);
+            }
+            match status {
+                DeStatus::Finished => break,
+                DeStatus::NeedInput => {
+                    if *this.flush {
+                        return Poll::Ready(Some(Err(Error::new(
+                            ErrorKind::UnexpectedEof,
+                            "reached unexpected EOF",
+                        ))));
+                    }
+                    break;
+                }
+                DeStatus::NeedOutput => (),
+            }
+        }
+
+        Poll::Ready(Some(Ok(decompressed_output.freeze())))
     }
 }
 
@@ -132,7 +131,7 @@ impl<S: Stream<Item = Result<Bytes>>> DecompressedBrotliStream<S> {
     pub fn new(stream: S) -> DecompressedBrotliStream<S> {
         DecompressedBrotliStream {
             inner: stream,
-            flushing: false,
+            flush: false,
             decompress: Decompress::new(),
         }
     }
--- src/stream/flate.rs
+++ src/stream/flate.rs
@@ -6,8 +6,8 @@ use std::{
 };
 
 use bytes::{Bytes, BytesMut};
-pub(crate) use flate2::Compress;
-use flate2::{FlushCompress, FlushDecompress, Status, Decompress};
+pub(crate) use flate2::{Compress, Decompress};
+use flate2::{FlushCompress, FlushDecompress, Status};
 use futures::{ready, stream::Stream};
 use pin_project::unsafe_project;
 
@@ -127,12 +127,11 @@ impl<S: Stream<Item = Result<Bytes>>> CompressedStream<S> {
 }
 
 #[unsafe_project(Unpin)]
-pub struct DecompressedStream<S: Stream<Item = Result<Bytes>>> {
+pub(crate) struct DecompressedStream<S: Stream<Item = Result<Bytes>>> {
     #[pin]
     inner: S,
-    flushing: bool,
-    input_buffer: Bytes,
-    output_buffer: BytesMut,
+    state: State,
+    output: BytesMut,
     decompress: Decompress,
 }
 
@@ -140,49 +139,94 @@ impl<S: Stream<Item = Result<Bytes>>> Stream for DecompressedStream<S> {
     type Item = Result<Bytes>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Result<Bytes>>> {
-        const OUTPUT_BUFFER_SIZE: usize = 8_000;
+        let mut this = self.project();
 
-        let this = self.project();
+        fn decompress(
+            decompress: &mut Decompress,
+            input: &mut Bytes,
+            output: &mut BytesMut,
+            flush: FlushDecompress,
+        ) -> Result<(Status, Bytes)> {
+            const OUTPUT_BUFFER_SIZE: usize = 8_000;
 
-        if this.input_buffer.is_empty() {
-            if *this.flushing {
-                return Poll::Ready(None);
-            } else if let Some(bytes) = ready!(this.inner.poll_next(cx)) {
-                this.input_buffer.extend_from_slice(&bytes?);
-            } else {
-                *this.flushing = true;
+            if output.len() < OUTPUT_BUFFER_SIZE {
+                output.resize(OUTPUT_BUFFER_SIZE, 0);
             }
+
+            let (prior_in, prior_out) = (decompress.total_in(), decompress.total_out());
+            let status = decompress.decompress(input, output, flush)?;
+            let input_len = decompress.total_in() - prior_in;
+            let output_len = decompress.total_out() - prior_out;
+
+            input.advance(input_len as usize);
+            Ok((status, output.split_to(output_len as usize).freeze()))
         }
 
-        this.output_buffer.resize(OUTPUT_BUFFER_SIZE, 0);
+        #[allow(clippy::never_loop)] // https://github.com/rust-lang/rust-clippy/issues/4058
+        loop {
+            break match mem::replace(this.state, State::Invalid) {
+                State::Reading => {
+                    *this.state = State::Reading;
+                    *this.state = match ready!(this.inner.as_mut().poll_next(cx)) {
+                        Some(chunk) => State::Writing(chunk?),
+                        None => State::Flushing,
+                    };
+                    continue;
+                }
 
-        let flush = if *this.flushing {
-            FlushDecompress::Finish
-        } else {
-            FlushDecompress::None
-        };
+                State::Writing(mut input) => {
+                    if input.is_empty() {
+                        *this.state = State::Reading;
+                        continue;
+                    }
 
-        let (prior_in, prior_out) = (this.decompress.total_in(), this.decompress.total_out());
-        this.decompress
-            .decompress(this.input_buffer, this.output_buffer, flush)?;
-        let input = this.decompress.total_in() - prior_in;
-        let output = this.decompress.total_out() - prior_out;
+                    let (status, chunk) = decompress(
+                        &mut this.decompress,
+                        &mut input,
+                        &mut this.output,
+                        FlushDecompress::None,
+                    )?;
 
-        this.input_buffer.advance(input as usize);
-        Poll::Ready(Some(Ok(this
-            .output_buffer
-            .split_to(output as usize)
-            .freeze())))
+                    *this.state = match status {
+                        Status::Ok => State::Writing(input),
+                        Status::StreamEnd => unreachable!(),
+                        Status::BufError => panic!("unexpected BufError"),
+                    };
+
+                    Poll::Ready(Some(Ok(chunk)))
+                }
+
+                State::Flushing => {
+                    let (status, chunk) = decompress(
+                        &mut this.decompress,
+                        &mut Bytes::new(),
+                        &mut this.output,
+                        FlushDecompress::Finish,
+                    )?;
+
+                    *this.state = match status {
+                        Status::Ok => State::Flushing,
+                        Status::StreamEnd => State::Done,
+                        Status::BufError => panic!("unexpected BufError"),
+                    };
+
+                    Poll::Ready(Some(Ok(chunk)))
+                }
+
+                State::Done => Poll::Ready(None),
+
+                State::Invalid => panic!("CompressedStream reached invalid state"),
+            };
+        }
     }
 }
 
 impl<S: Stream<Item = Result<Bytes>>> DecompressedStream<S> {
-    pub fn new(stream: S, decompress: Decompress) -> DecompressedStream<S> {
+    pub(crate) fn new(stream: S, decompress: Decompress) -> DecompressedStream<S> {
         DecompressedStream {
             inner: stream,
-            flushing: false,
-            input_buffer: Bytes::new(),
-            output_buffer: BytesMut::new(),
+            state: State::Reading,
+            output: BytesMut::new(),
             decompress,
         }
     }

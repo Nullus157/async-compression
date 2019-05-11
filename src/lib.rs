6eabe706c0cc853cae38224e6033244d25033fd4
Wim Looman <wim@nemo157.com>
2019-05-11 12:05:42 +0200

    Add basic top-level and module docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
+//! Adaptors between compression crates and Rust's modern asynchronous IO types.
+//!
+//!
+//! # Organization
+//!
+//! This crate is divided up into a number of modules based on the underlying asynchronous IO type
+//! that will be wrapped:
+//!
+//!  * [`read`] provides types which operate over [`AsyncBufRead`](futures::io::AsyncBufRead)
+//!    streams
+//!  * [`stream`] provides types which operate over [`Stream`](futures::stream::Stream)`<Item =
+//!    `[`io::Result`](std::io::Result)`<`[`Bytes`](bytes::Bytes)`>>` streams
+
 pub mod read;
 pub mod stream;
--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -1,3 +1,13 @@
+//! Types which operate over [`Stream`](futures::stream::Stream)`<Item =
+//! `[`io::Result`](std::io::Result)`<`[`Bytes`](bytes::Bytes)`>>` streams, both encoders and
+//! decoders for various formats.
+//!
+//! The `Stream` is treated as a single byte-stream to be compressed/decompressed, each item is a
+//! chunk of data from this byte-stream. There is not guaranteed to be a one-to-one relationship
+//! between chunks of data from the underlying stream and the resulting compressed/decompressed
+//! stream, the encoders and decoders will buffer the incoming data and choose their own boundaries
+//! at which to yield a new item.
+
 mod flate;
 
 pub mod brotli;

604ce0d9f46a8c02ff5658dbfa2f9853d0d85810
Wim Looman <wim@nemo157.com>
2019-09-18 11:41:59 +0200

    Try out zstd-rs decoder

--- src/codec/zstd/decoder.rs
+++ src/codec/zstd/decoder.rs
@@ -1,17 +1,18 @@
-use std::io::Result;
+use std::io::{Result, Error, ErrorKind};
 
-use crate::{codec::Decode, unshared::Unshared};
-use libzstd::stream::raw::{Decoder, Operation};
+use crate::codec::Decode;
+use zstd_rs::frame_decoder::FrameDecoder;
 
-#[derive(Debug)]
 pub struct ZstdDecoder {
-    decoder: Unshared<Decoder>,
+    decoder: FrameDecoder,
+    flushing: Option<Vec<u8>>,
 }
 
 impl ZstdDecoder {
     pub(crate) fn new() -> Self {
         Self {
-            decoder: Unshared::new(Decoder::new().unwrap()),
+            decoder: FrameDecoder::new(),
+            flushing: None,
         }
     }
 }
@@ -22,14 +23,30 @@ impl Decode for ZstdDecoder {
             return Ok((true, 0, 0));
         }
 
-        let status = self.decoder.get_mut().run_on_buffers(input, output)?;
-        Ok((false, status.bytes_read, status.bytes_written))
+        let (bytes_read, bytes_written) = self.decoder.decode_from_to(input, output).map_err(|e| Error::new(ErrorKind::Other, e))?;
+        Ok((self.decoder.is_finished(), bytes_read, bytes_written))
     }
 
     fn flush(&mut self, output: &mut [u8]) -> Result<(bool, usize)> {
-        let mut output = zstd_safe::OutBuffer::around(output);
+        if self.flushing.is_none() {
+            self.flushing = Some(self.decoder.drain_buffer());
+        }
 
-        let bytes_left = self.decoder.get_mut().flush(&mut output)?;
-        Ok((bytes_left == 0, output.as_slice().len()))
+        if let Some(flushing) = &mut self.flushing {
+            let len = std::cmp::min(flushing.len(), output.len());
+            output[..len].copy_from_slice(&flushing[..len]);
+            flushing.drain(..len);
+            Ok((flushing.is_empty(), len))
+        } else {
+            unreachable!()
+        }
+    }
+}
+
+impl core::fmt::Debug for ZstdDecoder {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("ZstdDecoder")
+            .field("decoder", &"[no debug]")
+            .finish()
     }
 }

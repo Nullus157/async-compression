6809a9a933ab1351d9f639d456eb477917783137
Wim Looman <wim@nemo157.com>
2019-10-13 13:31:18 +0200

    Mark currently inactive features in the docs

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,14 @@
 //! The first division is which underlying asynchronous IO type will be wrapped, these are
 //! available as two separate features that have corresponding top-level modules:
 //!
-//!  Feature                 | Type
-//! -------------------------|------
-//!  [`bufread`]             | [`futures::io::AsyncBufRead`](futures_io::AsyncBufRead)
-//!  [`write`](crate::write) | [`futures::io::AsyncWrite`](futures_io::AsyncWrite)
-//!  [`stream`]              | [`futures::stream::Stream`](futures_core::stream::Stream)`<Item = `[`std::io::Result`]`<`[`bytes::Bytes`]`>>`
+//!  Feature | Type
+//! ---------|------
+#![cfg_attr(feature = "bufread", doc = "[`bufread`] | [`futures::io::AsyncBufRead`](futures_io::AsyncBufRead)")]
+#![cfg_attr(not(feature = "bufread"), doc = "`bufread` (*inactive*) | `futures::io::AsyncBufRead`")]
+#![cfg_attr(feature = "write", doc = "[`write`](crate::write) | [`futures::io::AsyncWrite`](futures_io::AsyncWrite)")]
+#![cfg_attr(not(feature = "write"), doc = "`write` (*inactive*) | `futures::io::AsyncWrite`")]
+#![cfg_attr(feature = "stream", doc = "[`stream`] | [`futures::stream::Stream`](futures_core::stream::Stream)`<Item = `[`std::io::Result`]`<`[`bytes::Bytes`]`>>`")]
+#![cfg_attr(not(feature = "stream"), doc = "`stream` (*inactive*) | `futures::stream::Stream<Item = std::io::Result<bytes::Bytes>>`")]
 //!
 //!
 //! ## Compression implementation
@@ -30,12 +33,18 @@
 //! The second division is which compression scheme to use, there are currently a few available
 //! choices, these determine which types will be available inside the above modules:
 //!
-//!  * `brotli`
-//!  * `bzip`
-//!  * `deflate`
-//!  * `gzip`
-//!  * `zlib`
-//!  * `zstd`
+#![cfg_attr(feature = "brotli", doc = "* `brotli`")]
+#![cfg_attr(not(feature = "brotli"), doc = "* `brotli` (*inactive*)")]
+#![cfg_attr(feature = "bzip", doc = "* `bzip`")]
+#![cfg_attr(not(feature = "bzip"), doc = "* `bzip` (*inactive*)")]
+#![cfg_attr(feature = "deflate", doc = "* `deflate`")]
+#![cfg_attr(not(feature = "deflate"), doc = "* `deflate` (*inactive*)")]
+#![cfg_attr(feature = "gzip", doc = "* `gzip`")]
+#![cfg_attr(not(feature = "gzip"), doc = "* `gzip` (*inactive*)")]
+#![cfg_attr(feature = "zlib", doc = "* `zlib`")]
+#![cfg_attr(not(feature = "zlib"), doc = "* `zlib` (*inactive*)")]
+#![cfg_attr(feature = "zstd", doc = "* `zstd`")]
+#![cfg_attr(not(feature = "zstd"), doc = "* `zstd` (*inactive*)")]
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(

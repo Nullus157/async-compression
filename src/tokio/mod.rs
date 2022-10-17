e452a39a1c8ffc898d022b34eaa6a01a06721c88
Geoffroy Couprie <geoffroy@apollographql.com>
2022-10-17 14:24:11 +0200

    add a poll_flush implementation for the tokio bufread module

--- src/tokio/mod.rs
+++ src/tokio/mod.rs
@@ -2,3 +2,11 @@
 
 pub mod bufread;
 pub mod write;
+
+pub trait AsyncFlush {
+    fn poll_flush(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> std::task::Poll<std::io::Result<bool>>;
+}

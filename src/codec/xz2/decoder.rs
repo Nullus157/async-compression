34105953986f30f0331c6fc41c6ba0003ce6f2b1
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-06-10 00:25:23 +1000

    Fix clippy error in gzip decoder.rs
    Signed-off-by: Jiahao XU <30436523+NobodyXu@users.noreply.github.com>

--- src/codec/xz2/decoder.rs
+++ src/codec/xz2/decoder.rs
@@ -46,11 +46,10 @@ impl Decode for Xz2Decoder {
         match status {
             Status::Ok => Ok(false),
             Status::StreamEnd => Ok(true),
-            Status::GetCheck => Err(io::Error::new(
-                io::ErrorKind::Other,
-                "Unexpected lzma integrity check",
+            Status::GetCheck => Err(io::Error::other(
+                "Unexpected lzma integrity check"
             )),
-            Status::MemNeeded => Err(io::Error::new(io::ErrorKind::Other, "More memory needed")),
+            Status::MemNeeded => Err(io::Error::other("More memory needed")),
         }
     }
 

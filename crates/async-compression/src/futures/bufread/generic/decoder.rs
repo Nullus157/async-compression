bf4e41589701b96ad458645b18e06f3b4ec1f3b3
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-10-02 15:28:08 +0000

    Deduplicate `generic::bufread::Decoder` impl
    Ref #384

--- crates/async-compression/src/futures/bufread/generic/decoder.rs
+++ crates/async-compression/src/futures/bufread/generic/decoder.rs
@@ -1,22 +1,16 @@
 use crate::codecs::Decode;
 use crate::core::util::PartialBuffer;
+use crate::generic::bufread::{AsyncBufRead as GenericAsyncBufRead, Decoder as GenericDecoder};
 
 use core::{
     pin::Pin,
     task::{Context, Poll},
 };
+use std::io::{IoSlice, Result};
+
 use futures_core::ready;
 use futures_io::{AsyncBufRead, AsyncRead, AsyncWrite};
 use pin_project_lite::pin_project;
-use std::io::{IoSlice, Result};
-
-#[derive(Debug)]
-enum State {
-    Decoding,
-    Flushing,
-    Done,
-    Next,
-}
 
 pin_project! {
     #[derive(Debug)]
@@ -24,8 +18,7 @@ pin_project! {
         #[pin]
         reader: R,
         decoder: D,
-        state: State,
-        multiple_members: bool,
+        inner: GenericDecoder,
     }
 }
 
@@ -34,8 +27,7 @@ impl<R: AsyncBufRead, D: Decode> Decoder<R, D> {
         Self {
             reader,
             decoder,
-            state: State::Decoding,
-            multiple_members: false,
+            inner: GenericDecoder::default(),
         }
     }
 }
@@ -58,94 +50,31 @@ impl<R, D> Decoder<R, D> {
     }
 
     pub fn multiple_members(&mut self, enabled: bool) {
-        self.multiple_members = enabled;
+        self.inner.multiple_members(enabled);
     }
 }
 
 impl<R: AsyncBufRead, D: Decode> Decoder<R, D> {
-    fn do_poll_read(
+    pub(crate) fn do_poll_read(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         output: &mut PartialBuffer<&mut [u8]>,
     ) -> Poll<Result<()>> {
-        let mut this = self.project();
+        let this = self.project();
 
-        let mut first = true;
+        struct Reader<'a, R>(Pin<&'a mut R>);
 
-        loop {
-            *this.state = match this.state {
-                State::Decoding => {
-                    let input = if first {
-                        &[][..]
-                    } else {
-                        ready!(this.reader.as_mut().poll_fill_buf(cx))?
-                    };
-
-                    if input.is_empty() && !first {
-                        // Avoid attempting to reinitialise the decoder if the
-                        // reader has returned EOF.
-                        *this.multiple_members = false;
-
-                        State::Flushing
-                    } else {
-                        let mut input = PartialBuffer::new(input);
-                        let res = this.decoder.decode(&mut input, output).or_else(|err| {
-                            // ignore the first error, occurs when input is empty
-                            // but we need to run decode to flush
-                            if first {
-                                Ok(false)
-                            } else {
-                                Err(err)
-                            }
-                        });
-
-                        if !first {
-                            let len = input.written().len();
-                            this.reader.as_mut().consume(len);
-                        }
-
-                        first = false;
-
-                        if res? {
-                            State::Flushing
-                        } else {
-                            State::Decoding
-                        }
-                    }
-                }
-
-                State::Flushing => {
-                    if this.decoder.finish(output)? {
-                        if *this.multiple_members {
-                            this.decoder.reinit()?;
-                            State::Next
-                        } else {
-                            State::Done
-                        }
-                    } else {
-                        State::Flushing
-                    }
-                }
-
-                State::Done => State::Done,
-
-                State::Next => {
-                    let input = ready!(this.reader.as_mut().poll_fill_buf(cx))?;
-                    if input.is_empty() {
-                        State::Done
-                    } else {
-                        State::Decoding
-                    }
-                }
-            };
-
-            if let State::Done = *this.state {
-                return Poll::Ready(Ok(()));
+        impl<R: AsyncBufRead> GenericAsyncBufRead for Reader<'_, R> {
+            fn poll_fill_buf(&mut self, cx: &mut Context<'_>) -> Poll<Result<&[u8]>> {
+                self.0.as_mut().poll_fill_buf(cx)
             }
-            if output.unwritten().is_empty() {
-                return Poll::Ready(Ok(()));
+            fn consume(&mut self, bytes: usize) {
+                self.0.as_mut().consume(bytes)
             }
         }
+
+        this.inner
+            .do_poll_read(cx, output, &mut Reader(this.reader), this.decoder)
     }
 }
 

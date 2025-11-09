52ec3cd7c04dae34f1f7b406701b7a53b71c2870
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-11-09 03:37:56 +0000

    Refactor: Extract `tokio::poll_read`
    Extract duplicate code, easier to modify

--- crates/async-compression/src/tokio/bufread/generic/mod.rs
+++ crates/async-compression/src/tokio/bufread/generic/mod.rs
@@ -2,3 +2,26 @@ mod decoder;
 mod encoder;
 
 pub use self::{decoder::Decoder, encoder::Encoder};
+
+use crate::core::util::WriteBuffer;
+use std::{io::Result, task::Poll};
+use tokio::io::ReadBuf;
+
+fn poll_read(
+    buf: &mut ReadBuf<'_>,
+    do_poll_read: impl FnOnce(&mut WriteBuffer<'_>) -> Poll<Result<()>>,
+) -> Poll<Result<()>> {
+    if buf.remaining() == 0 {
+        return Poll::Ready(Ok(()));
+    }
+
+    let mut output = WriteBuffer::new_initialized(buf.initialize_unfilled());
+    match do_poll_read(&mut output)? {
+        Poll::Pending if output.written().is_empty() => Poll::Pending,
+        _ => {
+            let len = output.written_len();
+            buf.advance(len);
+            Poll::Ready(Ok(()))
+        }
+    }
+}
